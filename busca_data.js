f0f512af514b2626f0626f8d402b805ffe15565f
Camilla Correia <camillajesuscorreia@hotmail.com>
2024-02-12 19:51:26 -0300

    feat: search dates us

--- busca_data.js
+++ busca_data.js
@@ -2,6 +2,16 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-const regexData = /\d{2}[./ ]?\d{2}[./ ]?\d{4}$/gm
-const matchData = banco.match(regexData)
-console.log(matchData)
+const regexDate = /\d{2}[./ ]?\d{2}[./ ]?\d{4}$/gm
+const matchDate = banco.match(regexDate)
+console.log(matchDate)
+
+const dataString = "25/09/2023, 09/25/2023, 2023-09-25";
+const patternDateBr = /(\d{2}\/\d{2}\/\d{4})/g;
+const patternDateUs = /(\d{4}-\d{2}-\d{2})/g;
+
+const capturaDateBr = dataString.match(patternDateBr);
+console.log(capturaDateBr);
+
+const capturaDateUs= dataString.match(patternDateUs);
+console.log(capturaDateUs);
